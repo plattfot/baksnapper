fdbdcbaf83734f4c33ec1e5635bf156057f993df
Fredrik Salomonsson <plattfot@gmail.com>
2015-08-30 16:16:12 -0700

    Removing the old

--- snapperbak.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-
-if [[ ! -z "$1" ]]; then
-    subvolume=$(sudo snapper -c $1 get-config | grep SUBVOLUME | awk '{ print $3 }')
-    echo $subvolume
-else
-    echo "Usage: $0 <snapper config name>"
-fi
