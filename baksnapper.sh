e7ad7351836cd9c01a60b9b1e898511f2d60bbe8
Fredrik Salomonsson <plattfot@gmail.com>
2015-08-30 16:18:43 -0700

    Changing to bash

--- baksnapper.sh
+++ baksnapper.sh
@@ -1,8 +1,10 @@
-#! /bin/zsh
+#! /bin/bash
 
 if [[ ! -z "$1" ]]; then
     subvolume=$(sudo snapper -c $1 get-config | grep SUBVOLUME | awk '{ print $3 }')
     echo $subvolume
+    
+    
 else
     echo "Usage: $0 <snapper config name>"
 fi
