ce7f5440a51789f14e4a295dce30b40560b7190d
Fredrik Salomonsson <plattfot@gmail.com>
2015-08-30 17:51:32 -0700

    Added local backup support. Note haven't tested this on anything yet!

--- baksnapper.sh
+++ baksnapper.sh
@@ -5,18 +5,34 @@ help="Usage: $0 [OPTIONS]... [PATH]\n"\
 "Options:\n"\
 "\t-c <name>|--config=<name>\t Name of config.\n"\
 "\t-i |--init\t Initial backup, will send the whole snapshot.\n"\
+"\t-v |--verbose\t Verbose print out.\n"\
 "\t-h|--help\t Print this help and then exit.\n"\
 "Example:\n"\
 "\t$0 -c root /mnt/backup"\
 "Note:\n"\
 "This doesn't support option stacking e.g. -ic <name>. Instead you need to separate each option i.e. -i -c <name>\n"\
+"Also this need root to be able to backup"\
 "Exit status:\n"\
 " 0 if ok.\n"\
 " 1 if option error (e.g. wrong flag etc).\n"\
 ""
 
+function throw_error {
+    echo -e "[ERROR] $1"
+    exit 1
+}
+
+function printv {
+    if [[ $1 = 1 ]]; then
+        echo -e $2
+    fi
+}
+
+# Default values
 init=0
-#for i in "$@"
+verbose=0
+
+# Parse options
 while [[ $# > 0 ]]
 do
 key=$1
@@ -33,13 +49,17 @@ case $key in
         config="$2"
         shift 2
         ;;
+    -v|--verbose)
+        verbose=1
+        shift
+        ;;
     -h|--help)
         echo -e $help
         exit 0
         ;;
     -*)
         #unknown option
-        echo "Unknown option $1"
+        echo "[ERROR] Unknown option $1"
         echo -e $help
         exit 1
         shift
@@ -51,20 +71,56 @@ case $key in
 esac
 done
 
-echo "config=${config}"
-echo "dest=${dest}"
-echo "init=${init}"
+printv $verbose "config=${config}"
+printv $verbose "dest=${dest}"
+printv $verbose "init=${init}"
+
+# Error checks
 if [[ -z $config ]]; then
-    echo "you need to specify the config name to backup!"
-    echo -e $help
-    exit 1
+    throw_error "You need to specify the config name to backup!"
 fi
-# if [[ ! -z "$dest" ]]; then
+
+if [[ -z $dest ]]; then
+    throw_error "No path specified!"
+fi
+
+# Get the subvolume to backup
+subvolume=$(sudo snapper -c $config get-config | grep SUBVOLUME | awk '{ print $3 }')
+
+# List all the snapshots available
+snapshots=$(find $subvolume/.snapshots -mindepth 1 -maxdepth 1 -printf "%f ")
+snapshots_a=($snapshots)
+
+printv $verbose "snapshots=$snapshots"
+
+if [[ $init = 1 ]]; then
+    echo "Initialize backup"
+    snapshot=${snapshots_a[0]} 
     
-#     subvolume=$(sudo snapper -c $1 get-config | grep SUBVOLUME | awk '{ print $3 }')
-#     echo $subvolume
-    
-    
-# else
-#     echo "Usage: $0 <snapper config name>"
-# fi
+    printv $verbose "snapshot=$snapshot"
+    dest_dir=$dest/$config/$snapshot
+    src_dir=$subvolume/.snapshots/$snapshot
+
+    echo "mkdir $dest_dir"
+    echo "cp $src_dir/info.xml $dest_dir"
+    echo "sudo btrfs send $src_dir/snapshot | btrfs receive $dest_dir"
+else
+    echo "Incremental backup"
+    reference=${snapshots_a[0]}
+    num_snapshots=${#snapshots_a[@]}
+    snapshot=${snapshots_a[$num_snapshots-1]}
+
+    printv $verbose "reference=$reference"
+    printv $verbose "snapshot=$snapshot"
+
+    dest_dir=$dest/$config/$snapshot
+    src_dir=$subvolume/.snapshots/$snapshot
+    ref_dir=$subvolume/.snapshots/$reference
+    echo "mkdir $dest_dir"
+    echo "cp $src_dir/info.xml $dest_dir"
+    echo "sudo btrfs send -p $ref_dir/snapshot $src_dir/snapshot | btrfs receive $dest_dir"
+fi
+
+printv $verbose"subvolume=$subvolume"
+
+
