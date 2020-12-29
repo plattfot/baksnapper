91ee195cb64fba246e235021be4e6e60834351bf
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2020-12-29 15:30:22 -0600

    remove variable

--- baksnapper.sh
+++ baksnapper.sh
@@ -101,8 +101,6 @@ This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 EOF
 
-LINK=0
-
 function error {
     echo "[ERROR] $1" 1>&2
     exit 1
